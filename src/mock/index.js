8e3c9851a6a82df90b33e401c121ddd0c77d1075
wangyess <936886321@qq.com>
2019-03-13 17:25:00 +0800

    thr

--- src/mock/index.js
+++ src/mock/index.js
@@ -1,24 +1,24 @@
 const Mock = require('mockjs')
-const getDefaultValue = (option) => {
-  const data = {
-    name: 'wangye',
-    age:'23'
-  }
-  window.console.log(option)
-  return data
-}
-Mock.mock('http://www.baia.com/api/getDefaultValue', 'post', getDefaultValue);
-Mock.mock('http://www.bai.com', 'get', {
-  'list|50-100': [{
-    'numId': '@id()',
-    'id|+1': 1,
-    'roomNo': '@EMAIL(qq.com)',
-    'resort': '@cname',
-    'age|18-50': 25,
-    'roomClass': '@county(true)',
-    'yearStr': '@zip',
-    'businessDate': "@datetime('yy-MM-dd  HH:mm:ss')",
-    'guestType': '@guid()',
-    'packageCode': '@boolean()',
-  }]
+var Random = Mock.Random
+
+// 设置延迟时间
+Mock.setup({
+  timeout: 1000
 })
+
+//设置数据
+var defaultData = [];
+for (let i = 0; i < 4; i++){
+  let obj = {
+    'name': Random.cname(),
+    'email': Random.email('gmail.com')
+  }
+  defaultData.push(obj)
+}
+
+const getDefaultValue = (option) => {
+  window.console.log(option)
+  return defaultData
+}
+
+Mock.mock('http://www.baia.com/api/getDefaultValue', 'post', getDefaultValue);
